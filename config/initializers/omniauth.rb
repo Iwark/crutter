80f3e1171cbf96fb189c06efc7512f55bffe8c48
Iwark <iwark02@gmail.com>
2015-09-13 15:24:39 +0900

    Fix settings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.secrets.twitter_consumer_key, Rails.application.secrets.twitter_consumer_secret
+  setting = Setting.first
+  provider :twitter, setting.twitter_consumer_key, setting.twitter_consumer_secret
 end
